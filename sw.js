f8d7e6ed7dc6654167884cbfe43b45bc7d0de50a
ICIROJAS <113403918+ICIROJAS@users.noreply.github.com>
2025-02-11 15:32:27 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,42 +1,23 @@
-const CACHE_NAME = "to-do-list-cache-v1";
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./script.js",
-  "./icons/icon-192x192.png",
-  "./icons/icon-512x512.png"
-];
-
-// Instalar el Service Worker y cachear archivos
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
-
-// Activar el Service Worker y limpiar cache antigua
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
+self.addEventListener("install", function (event) {
+    event.waitUntil(
+        caches.open("app-cache").then(function (cache) {
+            return cache.addAll([
+                "index.html",
+                "script.js",
+                "style.css",
+                "manifest.json",
+                "icons/icon.png",           
+                "icons/icon-large.png"      
+            ]);
         })
-      );
-    })
-  );
+    );
 });
 
-// Interceptar solicitudes y servir desde cache
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+
+self.addEventListener("fetch", function (event) {
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            return response || fetch(event.request);
+        })
+    );
 });
